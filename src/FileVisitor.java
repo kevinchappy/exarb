c9060944569c06e2a0b75e70f9baa0de0441f12e
kevin <kevinqchapman@gmail.com>
2023-05-01 10:57:36 +0200

    Fixed errors - processes java-med without throwing exceptions

--- src/FileVisitor.java
+++ src/FileVisitor.java
@@ -1,5 +1,6 @@
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.expr.*;
@@ -17,7 +18,7 @@ import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class FileVisitor {
-    private final static String path = "C:/Users/kevin/Desktop/test files";
+    private final static String path = "C:/Users/kevin/Desktop/java-med";
     private static int bugCounter = 0;
     private static final Object bugCounterLock = new Object();
     private static int noBugCounter = 0;
@@ -25,24 +26,34 @@ public class FileVisitor {
 
     public static void main(String[] args) {
 
-        File root = new File(path);
-        List<File> javaFiles = getJavaFiles(root);
+        System.out.print("Finding all files");
+        List<File> javaFiles = extractJavaFiles(path);
+        System.out.println();
+        System.out.println("Found: " + javaFiles.size() + " files.");
+        System.out.println("Beginning to mutate methods");
 
         if (!javaFiles.isEmpty()) {
+            System.out.println("Starting timer");
+            long start = System.nanoTime();
             javaFiles.parallelStream().forEach(file -> {
                 try {
                     Printer printer = new DefaultPrettyPrinter();
-                    CompilationUnit cu = new JavaParser().parse(file).getResult().get();
-                    cu.accept(new MethodVisitor(), null);
-                    String modifiedCode = printer.print(cu);
-                    Files.write(Paths.get(file.getAbsolutePath()), modifiedCode.getBytes());
+                    CompilationUnit cu = new JavaParser().parse(file).getResult().orElse(null);
+                    if (cu != null) {
+                        cu.accept(new MethodVisitor(), null);
+                        String modifiedCode = printer.print(cu);
+                        Files.write(Paths.get(file.getAbsolutePath()), modifiedCode.getBytes());
+                    }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             });
+            long stop = System.nanoTime();
+
             System.out.println("bug count: " + bugCounter);
             System.out.println("nobug count: " + noBugCounter);
             System.out.println("Total : " + (bugCounter + noBugCounter));
+            System.out.println("Time taken: " + (stop - start) / 1.0E9);
         } else {
             System.out.println("Files not found");
         }
@@ -58,9 +69,9 @@ public class FileVisitor {
             Collections.addAll(directories, directoryList);
         }
 
-        for (File dir : directories){
+        for (File dir : directories) {
             File[] fileList = dir.listFiles();
-            if(fileList !=null)
+            if (fileList != null)
                 Collections.addAll(list, fileList);
         }
         return list;
@@ -77,7 +88,7 @@ public class FileVisitor {
                 return null;
             } else if (rand.nextBoolean()) {
                 method.setName("nobug");
-                synchronized (noBugCounterLock){
+                synchronized (noBugCounterLock) {
                     noBugCounter++;
                 }
             } else {
@@ -85,13 +96,23 @@ public class FileVisitor {
                 while (true) {
                     ForStmt forStmt = iter.next();
                     if (!iter.hasNext() || rand.nextBoolean()) {
-                        VariableDeclarationExpr varDeclExpr = (VariableDeclarationExpr) forStmt.getInitialization().get(0);
+                        VariableDeclarationExpr varDeclExpr = null;
+                        for (Node node : forStmt.getInitialization()) {
+                            if (node instanceof VariableDeclarationExpr) {
+                                varDeclExpr = (VariableDeclarationExpr) node;
+                            }
+                        }
+                        if (varDeclExpr == null) //remove method if it does not have a variable declaration, for example i = 0
+                            return null;
                         VariableDeclarator varDeclarator = varDeclExpr.getVariables().get(0);
+                        if (!(varDeclarator.getInitializer().isPresent() && varDeclarator.getInitializer().get().isIntegerLiteralExpr()))  //remove method if it does not use integer to initialize value
+                            return null;
+
                         IntegerLiteralExpr initialValue = (IntegerLiteralExpr) varDeclarator.getInitializer().get();
                         int newInitialValue = initialValue.asInt() + 1;
                         varDeclarator.setInitializer(new IntegerLiteralExpr(String.valueOf(newInitialValue)));
                         method.setName("bug");
-                        synchronized (bugCounterLock){
+                        synchronized (bugCounterLock) {
                             bugCounter++;
                         }
                         break;
@@ -109,9 +130,13 @@ public class FileVisitor {
                     if (statement instanceof ForStmt) {
                         ForStmt forStmt = (ForStmt) statement;
                         if (forStmt.getCompare().isPresent()) {
-                            BinaryExpr expr = (BinaryExpr) forStmt.getCompare().get();
-                            if (forStmt.getInitialization().size() == 1 && (expr.getRight().isFieldAccessExpr())) {
-                                list.add(forStmt);
+
+                            Expression expr = forStmt.getCompare().get();
+                            if (expr instanceof BinaryExpr) {
+                                BinaryExpr bExpr = (BinaryExpr) expr;
+                                if (forStmt.getInitialization().size() == 1 && (bExpr.getRight().isFieldAccessExpr())) {
+                                    list.add(forStmt);
+                                }
                             }
                         }
                     }
@@ -119,10 +144,25 @@ public class FileVisitor {
             }
             return list;
         }
-
     }
 
-
+    private static List<File> extractJavaFiles(String directoryPath) {
+        List<File> javaFiles = new ArrayList<>();
+        File directory = new File(directoryPath);
+        if (directory.isDirectory()) {
+            File[] files = directory.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isDirectory()) {
+                        javaFiles.addAll(extractJavaFiles(file.getAbsolutePath()));
+                    } else if (file.isFile() && file.getName().endsWith(".java")) {
+                        javaFiles.add(file);
+                    }
+                }
+            }
+        }
+        return javaFiles;
+    }
 }
 
 
